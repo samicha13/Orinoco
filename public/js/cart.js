287c07b751ded3593cbb81a4bf52a87dc39b2a6c
Samicha Pinson <samichapinson@gmail.com>
2021-05-06 10:13:53 +0200

    modification

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,7 +1,107 @@
-//Je récupére les élèments 
-const Panier = document.getElementById("teddyCard");
-const totalP = document.getElementById("totalPanier");
-const mainPanier = document.getElementById("mainPanier")
+let url = 'http://localhost:3000/api/teddies';
 
-const productsId = [];
+
+
+fetch(url) //recherche et récupération dans l'URL 
+    .then((response) => {
+        response.json().then((elements) => {
+
+            const main = document.getElementById('Test');
+            const row = document.createElement('div');
+            row.className = "row";
+            main.appendChild(row);
+
+    //Je récupére les élèments 
+    const Panier = document.getElementById("teddyCard");
+    const totalP = document.getElementById("totalPanier");
+    console.log(totalP);
+    const mainPanier = document.getElementById("mainPanier");
+    let total=0;
+
+    const productsId = [];
+    Object.entries(localStorage).forEach(e => {
+
+                // structure HTML 
+
+                //div col
+                const divCol = document.createElement("div");                 
+                divCol.className = 'col-12 col-md-6 col-lg-3 text-center';  
+                row.appendChild(divCol);  
+
+                //carte
+                const card = document.createElement('div')
+                card.className = 'card m-3 shadow-sm rounded-3';
+                divCol.appendChild(card);    
+
+                //image 
+                const cardImg = document.createElement('img');
+                cardImg.src = JSON.parse(e[1])["imageUrl"];
+                card.appendChild(cardImg); 
+
+                //caractéristiques ( nom+prix+Quantité)
+                const cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+                card.appendChild(cardBody);
+
+                //Nom de l'ours
+                const cardName = document.createElement('p');
+                cardName.className = 'card-title m-1';
+                cardBody.appendChild(cardName);
+                cardName.innerHTML = JSON.parse(e[1])["name"];  
+
+                // titre quantité
+                const quantity = document.createElement("p");
+                cardBody.appendChild(quantity);
+                quantity.textContent = "Quantité";
+                quantity.className = "quantity";
+                
+                //couleur
+const color = document.createElement("p");
+cardBody.appendChild(color);
+
+
+                // Quantité
+                const btnQuantity = document.createElement("input");
+                cardBody.appendChild(btnQuantity);
+                btnQuantity.setAttribute("id","quantite");
+                btnQuantity.setAttribute("type","number");
+                btnQuantity.setAttribute("value",JSON.parse(e[1])["qty"]);
+                btnQuantity.setAttribute("min",0);
+                btnQuantity.className = "quantityInput";
+
+                //Prix de l'ours
+                const cardPrice = document.createElement('p');
+                cardPrice.className = 'card-text m-1';
+                cardBody.appendChild(cardPrice);
+                cardPrice.innerHTML = 'Prix : ' + (JSON.parse(e[1])["price"])*(JSON.parse(e[1])["qty"]);
+                cardPrice.textContent = 'Prix : ' + (JSON.parse(e[1])["price"])*(JSON.parse(e[1])["qty"]) + ' ' + '€';
+
+                total=total+(JSON.parse(e[1])["price"])*(JSON.parse(e[1])["qty"]);
+                
+
+    })
+
+    const row2 = document.createElement('div');
+    row.className = "row2";
+    totalP.appendChild(row2);
+    row2.innerHTML = 'Total Panier est' + total +"€";
+    row2.textContent='Total de votre panier : ' + total +"€";
+
+    //Bouton 
+    const cardBtn = document.createElement('a');
+    cardBtn.href = "cart.html"          //lien pour récupérer l'id du produit
+    cardBtn.className = 'btn btn-dark m-1';
+    cardBtn.textContent = 'Supprimer';
+    row2.appendChild(cardBtn);
+
+    // element déclencheur
+    cardBtn.addEventListener("click",Delete) 
+
+    function Delete(event){
+        localStorage.clear();
+        }
+    })
+
+})
+
 
--- public/js/product.js
+++ public/js/product.js
@@ -1,191 +1,194 @@
 /*constante de Url*/
 
-const URL ="http://localhost:3000/api/teddies"
+const URL = "http://localhost:3000/api/teddies";
 
 
 
 
 //récupération de l'id  et des parametres passés dans l'url du produit 
-function getId(){
-	const param = window.location.search
-	const id = param.replace("?id=","") 
-	if (!id) throw new Error ("il manque l\'id")
-	return id
+function getId() {
+    const param = window.location.search
+    const id = param.replace("?id=", "")
+    if (!id) throw new Error("il manque l\'id")
+    return id
 }
 
 // recherche dans l'url
-fetch ("http://localhost:3000/api/teddies/" + getId())
-.then(windowProduct => windowProduct.json())
-.then(windowProduct => {console.log(windowProduct) 
+fetch("http://localhost:3000/api/teddies/" + getId())
+    .then(windowProduct => windowProduct.json())
+    .then(windowProduct => {
+        console.log(windowProduct)
 
 
-const oursContainer = document.getElementById("windowProduct");
-oursContainer.className =" bg-white m-3 shadow-sm"
+        const oursContainer = document.getElementById("windowProduct");
+        oursContainer.className = " bg-white m-3 shadow-sm row"
 
 
-function afficherLeProduit(Peluche) {
+        function afficherLeProduit(Peluche) {
 
-   /*structure HTML */
+            /*structure HTML */
 
-/*je crée 2 blocs pour pouvoir mettre l'image à côté du texte
+            /*je crée 2 blocs pour pouvoir mettre l'image à côté du texte
+            
+                /*création d'une div image pour y mettre la photo de l'ours*/
+            const divImg = document.createElement("div");
+            divImg.className = "col-12 col-md-8  text-center p-2 d-flex align-items-center"
+            oursContainer.appendChild(divImg);
 
-	/*création d'une div image pour y mettre la photo de l'ours*/
-		const divImg = document.createElement("div");
-    divImg.className = "col-12 col-md-6 text-center p-2 d-flex align-items-center "
-    oursContainer.appendChild(divImg);
-    
-		const imgProduct = document.createElement("img");   
-    imgProduct.src = Peluche.imageUrl;
-    imgProduct.className = "imgProduct";
-    divImg.appendChild(imgProduct);   
-
-
-    //div description du produit
-		const divProduct = document.createElement("div");
-    oursContainer.appendChild(divProduct);
-    divProduct.className = " col-12 col-md-6 p-0 text-center d-flex align-items-stretch ";
-    console.log(divProduct);
+            const imgProduct = document.createElement("img");
+            imgProduct.src = Peluche.imageUrl;
+            imgProduct.className = "imgProduct";
+            divImg.appendChild(imgProduct);
 
 
-//card du produit
-		const cardDescription = document.createElement("div");
-		divProduct.appendChild(cardDescription);
-    cardDescription.className = "border-0";
-
-    //caractéristique
-const cardBody = document.createElement("div");
-cardBody.className = "cardBody"
-cardDescription.appendChild(cardBody);
-
-//Nom de l'ours
-    const cardTitle = document.createElement("h2");
-    cardTitle.textContent = Peluche.name;
-    cardTitle.className = "cardTitle text-left card-text";
-    cardBody.appendChild(cardTitle);
-
-    //Description du produit (en tête)
-    const descrptionHead = document.createElement("p");
-    cardBody.appendChild(descrptionHead);
-    descrptionHead.className = "card-text text-left";
-    descrptionHead.textContent =" Description du produit";
-
-// div description contenu
-    const textProduct = document.createElement("p");
-    cardBody.appendChild(textProduct);
-    textProduct.textContent = Peluche.description;
-    textProduct.className = "textProduct";
-
-    //Prix produit
-     const cardPrice = document.createElement("p");
-    cardBody.appendChild(cardPrice);
-    cardPrice.textContent = `Prix : ${Peluche.price/100} €`;
-    cardPrice.className = "card-text m-2";
-
-     //const divPrice = document.createElement("div");
-    //blocProduct.appendChild(divPrice);
-    //divPrice.className = "divPrice";
+            //div description du produit
+            const divProduct = document.createElement("div");
+            oursContainer.appendChild(divProduct);
+            divProduct.className = " col-6 col-md-4  p-0 text-center d-flex align-items-stretch ";
+            console.log(divProduct);
 
 
-// option de la couleur des produits
+            //card du produit
+            const cardDescription = document.createElement("div");
+            divProduct.appendChild(cardDescription);
+            cardDescription.className = "border-0";
+
+            //caractéristique
+            const cardBody = document.createElement("div");
+            cardBody.className = "cardBody"
+            cardDescription.appendChild(cardBody);
+
+            //Nom de l'ours
+            const cardTitle = document.createElement("h2");
+            cardTitle.textContent = Peluche.name;
+            cardTitle.className = "cardTitle text-left card-text";
+            cardBody.appendChild(cardTitle);
+
+            //Description du produit (en tête)
+            const descrptionHead = document.createElement("p");
+            cardBody.appendChild(descrptionHead);
+            descrptionHead.className = "card-text text-left";
+            descrptionHead.textContent = " Description du produit";
+
+            // div description contenu
+            const textProduct = document.createElement("p");
+            cardBody.appendChild(textProduct);
+            textProduct.textContent = Peluche.description;
+            textProduct.className = "textProduct";
+
+            //Prix produit
+            const cardPrice = document.createElement("p");
+            cardBody.appendChild(cardPrice);
+            cardPrice.textContent = `Prix : ${Peluche.price / 100} €`;
+            cardPrice.className = "card-text m-2";
+
+            // option de la couleur des produits
 
 
-// div qui engloobe text et option
-const divSelect = document.createElement("div");
-divSelect.className = "d'flex justify-content-between justify-content-lg-around mt-3 mb-3'";
-cardBody.appendChild(divSelect);
+            // div qui engloobe text et option
+            const divSelect = document.createElement("div");
+            divSelect.className = "d'flex justify-content-between justify-content-lg-around mt-3 mb-3'";
+            cardBody.appendChild(divSelect);
 
 
 
-// Ajout du texte de choix d'une option
-    const option = document.createElement("p"); 
-    divSelect.appendChild(option);
-    option.textContent = "Choissisez la couleur de votre ours" + " ";
-    option.className = "option";
-    
-	//Création du formulaire qui contient les options
-      const select = document.createElement("select");
-      cardBody.appendChild(select);
+            // Ajout du texte de choix d'une option
+            const option = document.createElement("p");
+            divSelect.appendChild(option);
+            option.textContent = "Choissisez la couleur de votre ours" + " ";
+            option.className = "option";
 
-      // boucle pour afficher  toute les options
-      for (let color of windowProduct.colors){
-       
-        let option = document.createElement('option')
-        select.appendChild(option);
-        option.innerHTML = color;
-        select.setAttribute('value',color)
-        select.id = ('value', windowProduct.colors.value); 
-      }
+            //Création du formulaire qui contient les options
+            const select = document.createElement("select");
+            cardBody.appendChild(select);
 
-      // div regroupant nb ours et titre quantité
-    const oursQuantity = document.createElement("div");
-    cardBody.appendChild(oursQuantity);
-    oursQuantity.className = "oursquantity";
+            // boucle pour afficher  toute les options
+            for (let color of windowProduct.colors) {
 
-    // titre quantité
-    const quantity = document.createElement("p");
-    oursQuantity.appendChild(quantity);
-    quantity.textContent = "Quantité";
-    quantity.className = "quantity";
+                let option = document.createElement('option')
+                select.appendChild(option);
+                option.innerHTML = color;
+                select.setAttribute('value', color)
+                select.id = ('value', windowProduct.colors.value);
+            }
 
-    // choix du nombre d'ours voulu
-    const btnQuantity = document.createElement("input");
-    oursQuantity.appendChild(btnQuantity);
-    btnQuantity.setAttribute("type","number");
-    btnQuantity.setAttribute("value",1);
-    btnQuantity.setAttribute("min",0);
-    btnQuantity.className = "quantityInput";
+            // div regroupant nb ours et titre quantité
+            const oursQuantity = document.createElement("div");
+            cardBody.appendChild(oursQuantity);
+            oursQuantity.className = "oursquantity";
 
-    // création d' une div qui regroupe 2 boutons
-  	const divBtn = document.createElement("div");
-    cardBody.appendChild(divBtn);
-    divBtn.className = "divBtnAjouter";
+            // titre quantité
+            const quantity = document.createElement("p");
+            oursQuantity.appendChild(quantity);
+            quantity.textContent = "Quantité";
+            quantity.className = "quantity";
 
-    //bouton ajout panier
-    const btnAjout = document.createElement("button");
-    divBtn.appendChild(btnAjout);
-    btnAjout.className = "btn btn-outline-info m-2 p-2 cart_icon col-4"
-    btnAjout.textContent ="Ajouter";
-    btnAjout.setAttribute('role','button');
+            // choix du nombre d'ours voulu
+            const btnQuantity = document.createElement("input");
+            oursQuantity.appendChild(btnQuantity);
+            btnQuantity.setAttribute("type", "number");
+            btnQuantity.setAttribute("value", 1);
+            btnQuantity.setAttribute("min", 0);
+            btnQuantity.className = "quantityInput";
 
-    // bouton retour accueil
-    const btnReturn = document.createElement("a");
-    divBtn.appendChild(btnReturn);
-    btnReturn.className = "btn btn-outline-info shadow m-2 p2 col-7";
-    btnReturn.href = 'index.html';
-    btnReturn.setAttribute = ('role','button');
-    btnReturn.textContent = 'Retour à la lste des produits';
+            // création d' une div qui regroupe 2 boutons
+            const divBtn = document.createElement("div");
+            cardBody.appendChild(divBtn);
+            divBtn.className = "divBtnAjouter";
 
-   // element déclencheur
-    btnAjout.addEventListener("click",envoiDuProduit) 
+            //bouton ajout panier
+            const btnAjout = document.createElement("button");
+            divBtn.appendChild(btnAjout);
+            btnAjout.className = "btn btn-outline-info m-2 p-2 cart_icon col-4"
+            btnAjout.textContent = "Ajouter";
+            btnAjout.setAttribute('role', 'button');
 
-    function envoiDuProduit(event){
-    	//condition de sélectionner d'une quantité  si =0 envoyer une alerte     	
-      if (btnQuantity.value == 0 ){
-        alert("Veuillez sélectionner une quantité")
-        event.preventDefault();           
-      }
-      else{ // sinon on ajoute les elements id name price et description des que la qté est chosie
-       
-        cardPrice.textContent = ((Peluche.price / 100) * (+btnQuantity.value)) + "€";
-        
-			
-          let PelucheCart = {
-            id : Peluche._id,
-            name : Peluche.name,
-            price : Peluche.price/100,
-            description : Peluche.description,
-            imageUrl : Peluche.imageUrl,
-            select: select.value,	
-            qty : btnQuantity.value, 
-          };    
-			
-				//affiche dans le local storage
-			  	let PelucheOption = JSON.stringify(PelucheCart);
-        localStorage.setItem(Peluche._id, PelucheOption);
-        // alerte pour informer que le produit à bien été ajouté
-				alert (`${btnQuantity.value} ${ Peluche.name} ${select.value} ajouté au panier `);
-      } 
-    }
-  }
-  afficherLeProduit(windowProduct);   
-});
+            // bouton retour accueil
+            const btnReturn = document.createElement("a");
+            divBtn.appendChild(btnReturn);
+            btnReturn.className = "btn btn-outline-info shadow m-2 p2 col-7";
+            btnReturn.href = 'index.html';
+            btnReturn.setAttribute = ('role', 'button');
+            btnReturn.textContent = 'Retour à la liste des produits';
+
+            // element déclencheur
+            btnAjout.addEventListener("click", envoiDuProduit)
+
+            function envoiDuProduit(event) {
+                // A RAJOUTER faire en 2 temps voir dabord si le produit est present ou pas, s'il est pas présent on l'ajoute et s'il est present on l'incremente de btn qty value
+
+                //condition de sélectionner d'une quantité  si =0 envoyer une alerte     	
+                if (btnQuantity.value == 0) {
+                    alert("Veuillez sélectionner une quantité")
+                    event.preventDefault();
+                } else { // sinon on ajoute les elements id name price et description des que la qté est chosie
+
+
+
+                    cardPrice.textContent = ((Peluche.price / 100) * (+btnQuantity.value)) + "€";
+
+
+                    const pelucheCart = {
+                        id: Peluche._id,
+                        color: select.value,
+                        qty: btnQuantity.value,
+                    };
+
+                    const arrayPanier = JSON.parse(localStorage.getItem('Panier') )|| [];
+                    if (arrayPanier.some(panier => panier.id === pelucheCart.id)) { 
+                        console.log('deja present');
+                    } else {
+                    arrayPanier.push(pelucheCart);
+                    }
+
+                    console.log(arrayPanier);
+                    
+
+                    localStorage.setItem('Panier', JSON.stringify(arrayPanier));
+                    // alerte pour informer que le produit à bien été ajouté
+                    alert(`${btnQuantity.value} ${Peluche.name} ${select.value} ajouté au panier `);
+                }
+            }
+        }
+        afficherLeProduit(windowProduct);
+    });
